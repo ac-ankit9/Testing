d718064cb36cb26e1bd04d6f2be4ed156bf63aeb
[Ankit Chaudhary] <[ac.ankit9@gmial.com]>
2024-07-01 15:17:36 +0530

    repeated div

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -44,7 +44,57 @@ function Home() {
  </div>
 
  <div class="btm-section">
- <Card style={{ width: '18rem' }}>   
+  <Card style={{ width: '18rem' }}>   
+      <Card.Body>
+        <Card.Title>Web Design</Card.Title>
+        <Card.Text>
+        <ul>
+         <li>Small Business Website</li>
+         <li>Corporate Website</li>
+         <li>eCommerce Website</li>
+        </ul>  
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem'}}>   
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem'}}>   
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem'}}>   
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+ </div>
+ <div class="btm-section">
+  <Card style={{ width: '18rem' }}>   
       <Card.Body>
         <Card.Title>Web Design</Card.Title>
         <Card.Text>
